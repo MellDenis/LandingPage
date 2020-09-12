cb7aaaf5195c7b0ef6d40b52e5a8144010826aaf
MellDenis <meldperalta@gmail.com>
2020-09-11 21:42:24 -0300

    Riddle fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
 (function () {
     'use strict';
-    window.addEventListener('load', validation(), false); 
+    window.addEventListener('load', validation(), false);
 
 })();
 
@@ -28,7 +27,7 @@ function validation() {
         }, false);
 
     });
-  
+
 }
 
 //Riddle PLay
@@ -39,28 +38,26 @@ function playGame() {
     var numplayer;
 
     var name = document.getElementById("txtGamePlayer").value;
-    alert("Try to remember how many species of Graptopetalum there are ");
 
     if (name == "") {
         alert("Please enter a name to play :)");
     } else {
+        alert("Try to remember how many species of Graptopetalum there are. You have 3 times to try. ");
         while (attempts > 0 && success != true) {
             attempts--;
+            console.log(attempts);
             numplayer = parseInt(prompt("Enter a number"));
-            if (numplayer == numSucces) {
-                alert("You got it!  " + name );
-                success = true;
-            } else if (numplayer < numSucces) {
-                alert("Clues: it is higher \n  You have " + attempts + " attemps left");
-            } else if (numplayer > numSucces) {
-                alert("Clues: it is lower \n  You have " + attempts + " attemps left");
-            } else {
-                alert("Ohh! :( Maybe next time");
-            }
-        }   
-
-        alert("Ohh! :( Maybe next time");
-
+            if (isNaN(numplayer) == false) {
+                if (numplayer == numSucces) {
+                    alert("You got it " + name + "!");
+                    success = true;
+                } else if (numplayer < numSucces) {
+                    alert("Clues: it is higher \n  You have " + attempts + " attemps left");
+                } else if (numplayer > numSucces) {
+                    alert("Clues: it is lower \n  You have " + attempts + " attemps left");
+                } 
+            } 
+        }
         document.getElementById("successText").innerHTML = "The answer was: <strong>" + numSucces + "</strong>";
     }
 
@@ -69,13 +66,13 @@ function playGame() {
 //Alert create
 function createDivAndButton() {
     var newdiv = document.createElement('div');
-    newdiv.setAttribute('class','alert alert-info alert-dismissible fade show');
+    newdiv.setAttribute('class', 'alert alert-info alert-dismissible fade show');
     newdiv.innerHTML = "Your email has been sent :) !";
     var newButton = document.createElement('button');
     newButton.id = "alertButton";
-    newButton.setAttribute('class','close');
-    newButton.setAttribute('type','button');
-    newButton.setAttribute('data-dismiss','alert');
+    newButton.setAttribute('class', 'close');
+    newButton.setAttribute('type', 'button');
+    newButton.setAttribute('data-dismiss', 'alert');
     newButton.innerHTML = "&times;";
     var alertDiv = document.getElementById('divAlert');
     alertDiv.appendChild(newdiv);
@@ -85,9 +82,8 @@ function createDivAndButton() {
 //Execute alert
 function createAlert() {
     var buttonSubmit = document.getElementById("submit");
-    buttonSubmit.addEventListener("click",createDivAndButton());
+    buttonSubmit.addEventListener("click", createDivAndButton());
     console.log("Se creo el div");
 
-    
-};
 
+};
